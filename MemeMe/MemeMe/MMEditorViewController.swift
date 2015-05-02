af8f310c11a48aae040cb3b56d71420b03651cd9
Axel Guilmin <github@axelguilmin.fr>
2015-05-02 15:27:41 +0200

    First commit - created xcode project

---
+++ MemeMe/MemeMe/MMEditorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MMEditorViewController.swift
+//  MemeMe
+//
+//  Created by Axel Guilmin on 5/2/15.
+//  Copyright (c) 2015 Axel Guilmin. All rights reserved.
+//
+
+import UIKit
+
+class MMEditorViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
